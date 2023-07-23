8cae6f023def91d9d804df9f40771a1470e9584b
Hizas Sabilal <hizas.sabilal@gits.id>
2023-07-23 07:15:17 +0700

    feat: change response key data to result

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -4,18 +4,14 @@ const User = use('App/Models/User')
 
 class AuthController {
 
-  async register({ auth, request, response }){
+  async register({ request, response }){
     const body = request.only(['username', 'email', 'password'])
     try {
       const user = await User.create(body)
-      return response.status(201).json({
-        success: true,
-        data: { user: user },
-        message: 'User registered successfully',
-      });
+      return response.status(200).json({ success: true, result: user, message: 'User registered successfully' })
     } catch(error){
       console.error(error)
-      return response.status(400).json({ success: false, data: {}, message: 'Registration failed.' })
+      return response.status(400).json({ success: false, result: {}, message: 'Registration failed.' })
     }
   }
 
@@ -23,15 +19,13 @@ class AuthController {
     const body = request.only([ 'username', 'password' ])
     try {
       const token = await auth.attempt(body.username , body.password)
-      return response.status(200).json({ success: true, data: { token: token }, message: 'Logged in successfully.'
-      })
+      return response.status(200).json({ success: true, result: token, message: 'Logged in successfully.' })
     } catch (error) {
       console.error(error)
       if(error.name == 'UserNotFoundException' || error.name == 'PasswordMisMatchException') {
-        return response.status(401).json({ success: false, data: {}, message: 'Invalid credentials.'})
+        return response.status(401).json({ success: false, result: {}, message: 'Invalid credentials.'})
       }
-      else return response.status(500).json({ success: false, data: {}, message: 'Unknown error occured.'})
-      
+      else return response.status(500).json({ success: false, result: {}, message: 'Unknown error occured.'})
     }    
   }
 
